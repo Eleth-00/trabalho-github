760eab18022544324288568d9f1d806bbaaf50d1
jsantos <jair.jairs351@gmail.com>
2024-09-20 14:47:13 +0100

    retangulo implementado

--- Trabalho github/main.c
+++ Trabalho github/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+void AreaPerimetroDoRetangulo(){
+
+float width, height, area, perimeter;
+
+    printf("Indica a largura do retangulo:\n");
+    scanf("%f", &width);
+    printf("Indica a altura do retangulo:\n");
+    scanf("%f", &height);
+
+    area = width * height;
+    perimeter = 2* width + 2* height;
+
+    printf("A area do retangulo = %f\n", area);
+    printf("O perimetro do ratangulo = %f", perimeter);
 }
+
+
+
