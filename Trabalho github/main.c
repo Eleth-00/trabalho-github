ae013c43d5eb798a2a76f968b7a42ff15f247595
nlourenco <nuno_fgl@hotmail.com>
2024-09-20 13:01:54 +0100

    funcao circulo

--- Trabalho github/main.c
+++ Trabalho github/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+void circulo(){
+    float r;
+    int op;
+    do{
+        printf("Digite o raio do círculo:\n");
+        scanf("%f", &r);
+    }while (r <= 0);
+    printf("Qual a medida que precisa:\n\n");
+    do{
+        do{
+            printf("1 - Área.\n2 - Perímetro.\n3 - Saír.\n");
+            scanf("%d", &op);
+            getchar();
+        }while (op < 1 || op > 3);
+        switch (op){
+            case 1:
+                printf("A área é de %.2f cm2.", M_PI*pow(r,2));
+                printf("\nPrima qualquer tecla para continuar.");
+                getchar();
+                break;
+            case 2:
+                printf("O peímetro é de %.2f cm.", 2*M_PI*r);
+                printf("\nPrima qualquer tecla para continuar.");
+                getchar();
+                break;
+            case 3:
+                break;
+        }
+    system("cls");
+    }while (op != 3);
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
+    circulo();
+
+
     return 0;
 }
