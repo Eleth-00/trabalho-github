1d8832f3c56957f967882268dca631e6ab50c7a5
tpaulo <tatiisabelsp04@gmail.com>
2024-09-20 14:57:21 +0100

    multiplos 2, 3 e 5

--- Trabalho github/main.c
+++ Trabalho github/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+//funcao pentagono
+float area_perimetro_pentagono(){
+    float lado;
+    printf("Insira o valor do lado: ");
+    scanf("%f",&lado);
+    //perimetro
+    float perimetro = 5 * lado;
+    //apotema
+    float apotema = (lado /(2 * tan(M_PI /5)));
+    //area
+    float area = (perimetro * apotema) / 2;
+    printf("Perímetro do pentagono = %.2f",perimetro);
+    printf("\Área do pentagono = %.2f",area);
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
+    area_perimetro_pentagono();
     return 0;
 }
